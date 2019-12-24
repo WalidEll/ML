1b2462ab8d33df7323f7d7b1ec531cf34c4b90ab
WalidEll <walid.elalaouy@gmail.com>
2019-12-24 22:48:13 +0100

    simple perceptron

--- k_means/image_clustering.py
+++ k_means/image_clustering.py
@@ -10,10 +10,10 @@ imgplot = plt.imshow(catImage)
 x, y, z = catImage.shape
 image_2d = catImage.reshape(x*y, z)
 n=4
-kmeans_cluster = cluster.KMeans(n_clusters=n)
-kmeans_cluster.fit(image_2d)
+kmeans_cluster = cluster.KMeans(n_clusters=n).fit(image_2d)
 cluster_centers = kmeans_cluster.cluster_centers_
 cluster_labels = kmeans_cluster.labels_
 plt.imshow((cluster_centers[cluster_labels].reshape(x, y, z) * 255).astype(np.uint8))
 plt.savefig('clustered.png')
+print(cluster_labels.shape)
 plt.show()
